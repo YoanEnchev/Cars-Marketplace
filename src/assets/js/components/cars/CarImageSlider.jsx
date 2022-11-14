417193d3ad10e408868bba423d7f9c2a182c11cb
YoanEnchev <joanbojchev@gmail.com>
2022-11-14 23:21:23 +0200

    Thumbnail slider modal

--- src/assets/js/components/cars/CarImageSlider.jsx
+++ src/assets/js/components/cars/CarImageSlider.jsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react'
-import { Navigation, Pagination } from "swiper"
-import { Swiper, SwiperSlide } from "swiper/react"
+import React, { useState, useReducer } from 'react'
+import { Navigation, Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import ThumbnailSliderModal from './ThumbnailSliderModal'
 
 
 import "../../../scss/components/car-slider.scss"
@@ -10,17 +11,64 @@ import "swiper/css/navigation"
 import "swiper/css/pagination"
 
 export default function CarImageSlider(props) {
-    //const [cars, setCars] = useState([])
+    
+    const [swiper, setSwiper] = useState(null) // useState<Swiper>
+    const [showThumbnailModal, setShowThumbnailModal] = useState(false)
+    
+    // Changing active slide by using swiper.slideTo does not update component state.
+    // To do the rerendering, forceUpdate needs to be used.
+    // Use forceUpdate whenever slide is changed.
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
+
     const thumbnails = props.thumbnails
 
-    return <Swiper className='car-images-slider' 
+    function changeSlide(index) {
+        swiper.slideTo(index)
+    }
+
+    function openThumbnailModal() {
+        setShowThumbnailModal(true)
+    }
+
+    function closeThumbnailModal() {
+        setShowThumbnailModal(false)
+    }
+
+    return <> 
+        <Swiper className='car-images-slider'
             navigation={true}
             pagination={{clickable: true}} 
-            modules={[Navigation, Pagination]} >
+            modules={[Navigation, Pagination]}
+            onSwiper={(swiper) => setSwiper(swiper)}
+            onSlideChange={forceUpdate}
+        >
             
-            {thumbnails.map((thumbnailUrl, index) => 
-                <SwiperSlide key={index}>
-                    <img src={thumbnailUrl} className="w-100" />
-                </SwiperSlide>)}
-        </Swiper>  
+            {thumbnails.map((thumbnailUrl, index) => <SwiperSlide key={index}>
+                <img src={thumbnailUrl} className="w-100 slide-img" onClick={openThumbnailModal} />
+            </SwiperSlide>)}
+        </Swiper>
+        <div className='thumbnail-pagination'>
+            {thumbnails.map((thumbnailUrl, index) => {
+
+                // Assume active slide is at index 0 before it's initialized.
+                let activeSliderIndex = swiper === null ? 0 : swiper.activeIndex
+                let isActive = activeSliderIndex === index
+                
+                // Add background blending to the pagination image corresponding to the active slide.
+                let blendPercentage = isActive ? 34 : 0
+
+                return <div onClick={() => changeSlide(index)}
+                    key={index}
+                    className={'thumbnail-item d-inline-block mb-1 me-1' + (isActive ? ' active' : '')} 
+                    style={{background: `url(${thumbnailUrl}) rgb(21 21 34 / ${blendPercentage}%)`}} >
+                </div>
+            })}
+        </div>
+        <ThumbnailSliderModal thumbnails={thumbnails}
+            show={showThumbnailModal}
+            onHide={closeThumbnailModal}
+            onSlideChange={changeSlide}
+            activeSliderIndex={swiper === null ? 0 : swiper.activeIndex}
+         />
+    </>
 }
---
+++ src/assets/js/components/cars/ThumbnailSliderModal.jsx
@@ -0,0 +1,34 @@
+import Modal from 'react-bootstrap/Modal'
+
+import React, { useState } from 'react'
+import { Navigation } from "swiper"
+import { Swiper, SwiperSlide } from "swiper/react"
+
+export default function ThumbnailSliderModal(props) {
+
+    const [swiper, setSwiper] = useState(null) // useState<Swiper>
+  
+    function handleClose() {
+      props.onHide()
+    }
+  
+    return (
+      <Modal show={props.show} onHide={handleClose} className='thumbnail-slider-modal modal-xl'>
+        <Modal.Header closeButton></Modal.Header>
+        <Modal.Body>
+        <Swiper className='car-images-slider-modal'
+            navigation={true} 
+            modules={[Navigation]}
+            onSwiper={(swiper) => setSwiper(swiper)}
+            onSlideChange={() => props.onSlideChange(swiper.activeIndex)}
+            initialSlide={props.activeSliderIndex}
+        >
+            
+            {props.thumbnails.map((thumbnailUrl, index) => <SwiperSlide key={index}>
+                <img src={thumbnailUrl} className="w-100" />
+            </SwiperSlide>)}
+        </Swiper>
+        </Modal.Body>
+      </Modal>
+    )
+  }
