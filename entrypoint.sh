b5f629df119d92ebb8caa8fdab36ec22e7250662
YoanEnchev <joanbojchev@gmail.com>
2023-03-28 00:16:46 +0300

    new logic for entrypoint.sh and gitignore files.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,21 @@
 cd ./assets/js
-npm install
-npm run build
 
+if ! [ -d "node_modules" ]; then
+    # There is no node_modules folder. So install modules.
+    npm install
+fi
+
+
+if ! [[ -f "../../static/js/app.bundle.js" ]]; then
+    # There is no bundle file. So build it.
+    npm run build
+fi
 
 # Go back to /web_app:
 cd ../../
 
 echo '..........................................'
-echo 'Finished building. Server is up'
+echo 'Finished building. Server is up.'
 echo '..........................................'
 
 apache2ctl -D FOREGROUND
